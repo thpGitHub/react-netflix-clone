9b3231f9322c44143802c9dd0d4d89692aa78aad
thierry <thierrypoupon@hotmail.com>
2022-06-06 21:31:52 +0200

    working on navbar mui

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+// import {ThemeProvider} from '@mui/styles'
 import NetflixApp from './components/NetflixApp'
 
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+    background: '#111',
+//   palette: {
+//     primary: {
+//       main: red[500],
+//     },
+//   },
+});
+
+
+// const theme = {
+//     background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+// }
+
 function App() {
-    return <NetflixApp />
+    return (
+        <ThemeProvider theme={theme}>
+            <NetflixApp />
+        </ThemeProvider>
+    )
 }
 
 export default App
