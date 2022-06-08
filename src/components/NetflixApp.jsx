36411bbb1970991b4efe4474d86157737c48d4b5
thierry <thierrypoupon@hotmail.com>
2022-06-08 17:28:49 +0200

    navbar style on sroll

--- src/components/NetflixApp.jsx
+++ src/components/NetflixApp.jsx
@@ -1,14 +1,37 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
 import './Netflix.css'
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
 
 const NetflixApp = () => {
-    const appBarStyle = {
+    const [appBarStyle, setAppBarStyle] = useState({
         background: 'transparent',
         boxShadow: 'none',
-    }
+    })
+
+    useEffect(() => {
+        const onScroll = e => {
+            console.log(e.target.documentElement.scrollTop)
+            if (e.target.documentElement.scrollTop > 100) {
+                setAppBarStyle({
+                    boxShadow: 'none',
+                    background: '#111',
+                    transition: 'background 5s ease-out',
+                })
+            } else {
+                setAppBarStyle({
+                    boxShadow: 'none',
+                    background: 'transparent',
+                    transition: 'background 5s ease-out',
+                })
+            }
+        }
+        window.addEventListener('scroll', onScroll)
+
+        return () => window.removeEventListener('scroll', onScroll)
+    }, [])
+
     const margin10 = {margin: 10}
 
     return (
