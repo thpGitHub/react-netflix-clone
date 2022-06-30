4e8d65fbdb81c0dba8239d53fede5031ab0a7f9f
thierry <thierrypoupon@hotmail.com>
2022-06-30 16:38:00 +0200

    working in progress on fetch api

--- src/components/NetflixApp.tsx
+++ src/components/NetflixApp.tsx
@@ -7,35 +7,28 @@ import NetflixAppBar from './NetflixAppBar'
 import NetflixHeader from './NetflixHeader'
 import NetflixFooter from './NetflixFooter'
 
-import { getRandomType, getRandomId } from '../utils/helper'
-import { clientApi } from '../utils/clientAPI'
-import { API_KEY, LANG} from '../const'
+import {getRandomType, getRandomId} from '../utils/helper'
+import {clientApi} from '../utils/clientAPI'
+import {API_KEY, LANG} from '../const'
 
-import axios from 'axios'
-import {AxiosResponse} from '../ts/interfaces/axiosResponse'
+import axios, {AxiosResponse} from 'axios'
+// import {AxiosResponse} from '../ts/interfaces/axiosResponse'
 
 const NetflixApp = () => {
+    // const [headerMovie, setHeaderMovie] = useState<AxiosResponse | null | void>()
     const [headerMovie, setHeaderMovie] = useState<AxiosResponse>()
     const [type] = useState(getRandomType())
-  
+
     const defaultMovieId = getRandomId(type)
 
     // const apiKey = '4fc7b001e8a107fe1fddc6b41ed0f4af'
-   
+
     useEffect(() => {
-        const fetchMovie = async () => {
-            try {
-                // clientApi(`${type}${defaultMovieId}`)
-                const response = await axios.get(
-                    `https://api.themoviedb.org/3/${type}/${defaultMovieId}?api_key=${API_KEY}&language=${LANG}`,
-                ) as AxiosResponse
-                console.log('response', response)
+        clientApi(`${type}/${defaultMovieId}`)
+            .then(response => {
                 setHeaderMovie(response)
-            } catch (error) {
-                console.error(error)
-            }
-        }
-        fetchMovie()
+            })
+            .catch(error => console.error(error))
     }, [])
 
     return (
@@ -53,4 +46,3 @@ const NetflixApp = () => {
 export default NetflixApp
 
 // https://api.themoviedb.org/3/movie/550?api_key=8ee18f65008b7108b46834a1a60f55fc
-
--- src/utils/clientAPI.ts
+++ src/utils/clientAPI.ts
@@ -7,5 +7,23 @@ const clientApi = (endpoint: string) => {
   const keyLang = `${startChar}api_key=${API_KEY}&language=${LANG}&page=${page}`
   return axios.get(`${API_URL}/${endpoint}${keyLang}`)
 }
+// const clientApi = async (endpoint: string) => {
+//   const page = 1
+//   // const startChar = endpoint.includes('?') ? `&` : `?`
+//   // const keyLang = `${startChar}api_key=${API_KEY}&language=${LANG}&page=${page}`
+//   const keyLang = `?api_key=${API_KEY}&language=${LANG}&page=${page}`
+//   return axios.get(`${API_URL}/${endpoint}${keyLang}`)
+// }
 
 export {clientApi}
+
+// const fetchComic = async id => {
+//   try {
+//       const response = await axios.get(
+//           `https://thierry-api-marvel.herokuapp.com/comic/${id}`,
+//       )
+//       return await response.data
+//   } catch (error) {
+//       console.log(error)
+//   }
+// }
