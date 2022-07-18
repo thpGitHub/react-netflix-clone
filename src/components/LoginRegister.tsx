bad17b2eb65383cb3f48bc31fdf26ed43e70782f
thierry <thierrypoupon@hotmail.com>
2022-07-18 19:19:24 +0200

    working on component LoginRegister

--- src/components/LoginRegister.tsx
+++ src/components/LoginRegister.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Button from '@mui/material/Button'
 import TextField from '@mui/material/TextField'
 import Dialog from '@mui/material/Dialog'
@@ -10,69 +10,96 @@ import FormGroup from '@mui/material/FormGroup'
 import FormControlLabel from '@mui/material/FormControlLabel'
 import Checkbox from '@mui/material/Checkbox'
 
-const LoginRegister = () => {
-    const [open, setOpen] = React.useState(true)
-
-    const handleClickOpen = () => {
-        setOpen(true)
-    }
-
-    // const handleClose = () => {
-    //     setOpen(false)
-    // }
+const FormLogin = ({createLoginCount = true}) => {
+    const labelButton = createLoginCount ? 'Inscrivez vous' : 'Connexion'
+    const labelButtonInfoCount = createLoginCount
+        ? 'Vous posséder déjà un compte ?'
+        : 'Nouveau sur Netflix ?'
 
     return (
-        <div>
-            {/* <Button variant="outlined" onClick={handleClickOpen}>
-                Open form dialog
-            </Button> */}
-            {/* <Dialog open={open} onClose={handleClose}> */}
-            <Dialog open={open}>
-                <DialogTitle>Connexion</DialogTitle>
-                <DialogContent>
-                    <TextField
-                        autoFocus
-                        margin="dense"
-                        id="name"
-                        label="Email ou numéro de téléphone"
-                        type="email"
-                        fullWidth
-                        variant="filled"
-                    />
-                    <TextField
-                        margin="dense"
-                        id="name"
-                        label="Mot de passe"
-                        type="password"
-                        fullWidth
-                        variant="filled"
-                    />
-                </DialogContent>
-                <DialogActions
+        <form autoComplete="off">
+            <TextField
+                autoFocus
+                margin="dense"
+                id="filled-basic-username"
+                label="Email ou numéro de téléphone"
+                type="email"
+                fullWidth
+                variant="filled"
+            />
+            <TextField
+                margin="dense"
+                id="filled-basic-password"
+                label="Mot de passe"
+                type="password"
+                fullWidth
+                variant="filled"
+            />
+            <Button variant="contained" fullWidth>
+                {labelButton}
+                {/* CONNEXION */}
+            </Button>
+            <FormGroup>
+                <FormControlLabel
                     style={{
-                        justifyContent: 'center',
+                        justifyContent: 'flex-start',
                         paddingLeft: '24px',
                         paddingRight: '24px',
                     }}
+                    control={<Checkbox defaultChecked />}
+                    label="Se souvenir de moi"
+                />
+            </FormGroup>
+            {/* <Button
+                style={{
+                    justifyContent: 'flex-start',
+                    marginTop: '24px',
+                }}
+            >
+                {labelButtonInfoCount}
+            </Button> */}
+        </form>
+    )
+}
+
+const PopupLogin = ({signUp = false}) => {
+    const [createLogin, setCreateLogin] = useState(signUp)
+    const [open, setOpen] = React.useState(true)
+
+    const handleSignUp = () => {
+        setCreateLogin(true)
+    }
+    const handleSignIn = () => {
+        setCreateLogin(false)
+    }
+
+    return (
+        <div>
+            <Dialog open={open} style={{backgroundColor: 'transparent'}}>
+                <DialogTitle>Connexion</DialogTitle>
+                <DialogContent>
+                    <FormLogin />
+                </DialogContent>
+                <DialogActions
+                    style={{
+                        justifyContent: 'flex-start',
+                    }}
                 >
-                    <Button variant="contained" fullWidth>
-                        {' '}
-                        CONNEXION
-                    </Button>
+                    {!createLogin ? (
+                        <Button onClick={handleSignUp}>
+                            {/* Nouveau sur Netflix ? {spinner} */}
+                            Nouveau sur Netflix ?
+                        </Button>
+                    ) : (
+                        <Button onClick={handleSignIn}>
+                            {/* Vous posséder déjà un compte ? {spinner} */}
+                            Vous posséder déjà un compte ?
+                        </Button>
+                    )}
                 </DialogActions>
-                <FormGroup>
-                    <FormControlLabel
-                        control={<Checkbox defaultChecked />}
-                        label="Se souvenir de moi"
-                    />
-                </FormGroup>
-                <Button fullWidth>
-                        {' '}
-                        Nouveau sur Netflix ?
-                    </Button>
             </Dialog>
         </div>
     )
 }
 
-export default LoginRegister
+export default PopupLogin
--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import LoginRegister  from './components/LoginRegister'
+import PopupLogin  from './components/LoginRegister'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
     // <App />
-    <LoginRegister />
+    <PopupLogin />
   // </React.StrictMode>
 );
 
