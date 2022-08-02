1e51d7c28a713b6841c1315efd75723050f8f400
thierry <thierrypoupon@hotmail.com>
2022-08-02 11:52:17 +0200

    working in mock Db

--- src/mocks/db.tsx
+++ src/mocks/db.tsx
@@ -2,15 +2,46 @@
    dans le localStorage
 */
 
-const localStorageKey = 'netflix2222-clone-users'
+// const localStorageKey = 'netflix2222F-clone-users'
+const localStorageKey = 'netflix-clone-users'
 
-const loadUser = async () => {
-    const users = localStorage.getItem(localStorageKey)
-    return users ?? []
+const loadUsersFromLocalStorage = async () => {
+    // const users  = JSON.parse(localStorage.getItem(localStorageKey) || "false")
+    let users  = localStorage.getItem(localStorageKey)
+
+    if (typeof users === 'string') {
+        // const parse = JSON.parse(value) // ok
+        console.log('users STRING in loadUserFromLocalStorage', users)
+        // users = JSON.parse(users)
+        return users = JSON.parse(users)
+    
+    }
+
+    console.log('users in loadUserFromLocalStorage', users)
+
+    // return users ?? 12
+    return users
+    // return users || []
 }
 
-const saveUserInlocalStorage = (user: {id: string; userName: string; password: string}) => {
-    localStorage.setItem(localStorageKey, JSON.stringify(user))
+const saveUserInlocalStorage = async (user: {
+    id: string
+    userName: string
+    password: string
+}) => {
+    const users = await loadUsersFromLocalStorage()
+
+    console.log('users in saveUserInlocalStorage', users);
+    
+
+    if(users) {
+        users.push(user)
+        // console.log('user', user);
+        localStorage.setItem(localStorageKey, JSON.stringify(users))
+        
+    }
+
+    // localStorage.setItem(localStorageKey, JSON.stringify(user))
 }
 
 const createUser = async ({
@@ -24,6 +55,7 @@ const createUser = async ({
 
     const user = {id, userName, password}
     saveUserInlocalStorage(user)
+    // loadUsersFromLocalStorage()
 }
 
 export {createUser}
