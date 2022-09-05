b3ad874dea4016ea348d761942a9ae3ce14d3f9f
thierry <thierrypoupon@hotmail.com>
2022-09-05 15:13:35 +0200

    working on mock db

--- src/UnauthApp.tsx
+++ src/UnauthApp.tsx
@@ -18,21 +18,28 @@ interface IProps {
 }
 
 const UnauthApp = ({login, register}: IProps) => {
-    const imageURL = 'images/posters.jpg'
+    const imageURL = '/images/posters.jpg'
 
     return (
         <div
             style={{
-                backgroundImage: `url('${imageURL}')`,
-                backgroundSize: 'cover',
-                position: 'fixed',
                 top: 0,
                 left: 0,
-                bottom: 0,
                 right: 0,
+                bottom: 0,
                 overflow: 'auto',
+                position: 'fixed',
+                backgroundSize: 'cover',
+                backgroundImage: `url('${imageURL}')`,
             }}
         >
+            <img
+                src="/images/netflix-logo.png"
+                alt=""
+                style={{margin: '30px'}}
+                height={50}
+            />
+
             <LoginRegister login={login} register={register} />
         </div>
     )
