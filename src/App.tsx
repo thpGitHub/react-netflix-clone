d0483d38f3641cf4551f2c8bdd6c685b1d338c83
thierry <thierrypoupon@hotmail.com>
2022-09-26 15:38:55 +0200

    add dev tool : ReactQueryDevtools

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import {ThemeProvider, createTheme} from '@mui/material/styles'
 import Backdrop from '@mui/material/Backdrop'
 import CircularProgress from '@mui/material/CircularProgress'
 // ** REACT Query
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
 
 const queryClient = new QueryClient()
@@ -117,6 +118,7 @@ function App() {
                     />
                 )}
             </ThemeProvider>
+            <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
     )
 }
