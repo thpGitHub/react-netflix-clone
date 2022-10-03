fd5f90de12cb7275191d6b856cf9e74783e88889
thierry <thierrypoupon@hotmail.com>
2022-10-03 10:24:33 +0200

    working in progress on errorBoundary

---
+++ src/components/ErrorFallback.js
@@ -0,0 +1,41 @@
+import {NetflixAppBar} from 'components/NetflixAppBar'
+import { useNavigate  } from "react-router-dom"
+
+function ErrorFallback({error, resetErrorBoundary}) {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/")
+    resetErrorBoundary()
+  }
+  return (
+    <div>
+      <NetflixAppBar />
+      <div
+        role="alert"
+        style={{
+          height: '100%',
+          textAlign: 'center',
+          margin: '100px 300px',
+          color: '#fff',
+        }}
+      >
+        <h1 style={{fontSize: '2.5em'}}>Vous cherchez votre chemin ?</h1>
+        <pre style={{color: 'red', fontSize: '1em'}}>
+          Erreur : {error.message}
+        </pre>
+
+        <div className="banner__buttons">
+          <button
+            className="banner__button banner__buttonplay"
+            onClick={handleClick}
+          >
+            Accueil
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ErrorFallback
