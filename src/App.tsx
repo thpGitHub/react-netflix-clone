44e2b32a9d57fa1a8ed7a8cd6e198a4a0143baf0
thierry <thierrypoupon@hotmail.com>
2022-10-04 12:56:34 +0200

    add defaultOption in queryClient

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,30 @@ import CircularProgress from '@mui/material/CircularProgress'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
 
-const queryClient = new QueryClient()
+// const queryClient = new QueryClient()
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        useErrorBoundary: true,
+        refetchOnWindowFocus: false,
+        retryDelay: 500,
+        retry: (failureCount, error: any) => {
+          if (error.status === 404) return false
+          else if (error.status === 401) return false
+          else if (failureCount > 3) return false
+          else return true
+        },
+      },
+      mutations: {
+        useErrorBoundary: false,
+        // refetchOnWindowFocus: false,
+        retryDelay: 500,
+        retry: 1,
+        // mutation options
+      },
+    },
+  })
 
 const theme = createTheme({
     palette: {
