7d1abc73e8824600a73337820313063cbf1ddd11
thierry <thierrypoupon@hotmail.com>
2022-10-10 17:52:45 +0200

    working on custom hooks for output common states

--- src/utils/hooksMovies.tsx
+++ src/utils/hooksMovies.tsx
@@ -8,4 +8,64 @@ const useMovie = (TYPE_MOVIE: string, id: number) => {
     return data
 }
 
-export {useMovie}
+const useMovieEndpoint = (type: string, filter: string, param: string) => {
+    const endpointLatest = `${type}/upcoming`
+    const endpointPopular = `${type}/popular`
+    const endpointTopRated = `${type}/top_rated`
+    const endpointGenre = `discover/${type}?with_genres=${param}`
+    const endpointTrending = `trending/${type}/day`
+
+    let endpoint: string = ''
+
+    switch (filter) {
+        case 'populaire':
+            endpoint = endpointPopular
+            break
+        case 'latest':
+            endpoint = endpointLatest
+            break
+        case 'toprated':
+            endpoint = endpointTopRated
+            break
+        case 'genre':
+            endpoint = endpointGenre
+            break
+        case 'trending':
+            endpoint = endpointTrending
+            break
+
+        default:
+            throw new Error('Type non supporté')
+    }
+
+    const {data} = useQuery([`${endpoint}`], () => clientApi(endpoint))
+
+    return data
+}
+
+/**
+ * This function is two fold in App.tsx
+ */
+//  const getUserByToken = async () => {
+//     let user = null
+//     const token = await authNetflix.getTokenInLocalStorage()
+
+//     if (token) {
+//         console.log('Token exist :)')
+//         const data = await clientAuth('getUserAuth', token)
+//         // AxiosResponse
+//         user = data.data.user
+//         console.log('data ====', data)
+//         console.log('user ====', user)
+//     }
+
+//     return user
+// }
+
+// const useBookmark = () => {
+//     const {data} = useQuery(['bookmark'], () => {
+//         return getUserByToken()
+//     })
+// }
+
+export {useMovie, useMovieEndpoint}
