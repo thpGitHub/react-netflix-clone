edd8dc9424c79b73a7b2d6c7854c1a3c64230f92
thierry <thierrypoupon@hotmail.com>
2022-11-09 14:26:16 +0100

    add index.tsx in folder contexts for go out all providers from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,61 +4,15 @@ import './mocks'
 // ** Components **
 import AuthApp from './AuthApp'
 import UnauthApp from './UnauthApp'
-// ** MUI **
-import {ThemeProvider, createTheme} from '@mui/material/styles'
-// ** REACT Query
-import {ReactQueryDevtools} from '@tanstack/react-query-devtools'
-import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
-// ** Contexts
-import {AuthContextProvider, useAuthContext} from './contexts/authContext'
-
-const queryClient = new QueryClient({
-    defaultOptions: {
-        queries: {
-            useErrorBoundary: true,
-            refetchOnWindowFocus: false,
-            retryDelay: 500,
-            retry: (failureCount, error: any) => {
-                if (error.status === 404) return false
-                else if (error.status === 401) return false
-                else if (failureCount > 3) return false
-                else return true
-            },
-        },
-        mutations: {
-            useErrorBoundary: false,
-            // refetchOnWindowFocus: false,
-            retryDelay: 500,
-            retry: 1,
-            // mutation options
-        },
-    },
-})
-
-const theme = createTheme({
-    palette: {
-        // mode: 'dark',
-        primary: {
-            main: '#E50914',
-        },
-        secondary: {
-            main: '#E50914',
-        },
-    },
-})
+// ** Contexts **
+import {useAuthContext} from './contexts/authContext'
+import AppProviders from './contexts'
 
 function App() {
     return (
-        <QueryClientProvider client={queryClient}>
-            <ThemeProvider theme={theme}>
-                {/* <AuthContext.Provider value={props}> */}
-                <AuthContextProvider>
-                    <AppConsumer />
-                </AuthContextProvider>
-                {/* </AuthContext.Provider> */}
-            </ThemeProvider>
-            <ReactQueryDevtools initialIsOpen={false} />
-        </QueryClientProvider>
+        <AppProviders>
+            <AppConsumer />
+        </AppProviders>
     )
 }
 
