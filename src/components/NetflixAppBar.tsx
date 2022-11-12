06b2ff283648149ca85594a2c5029033d2bf99a8
thierry <thierrypoupon@hotmail.com>
2022-11-12 18:32:05 +0100

    working on netflixSearch component and add search in appbar

--- src/components/NetflixAppBar.tsx
+++ src/components/NetflixAppBar.tsx
@@ -1,18 +1,66 @@
 import React, {useState, useEffect} from 'react'
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 // ** MUI **
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
+import {styled, alpha} from '@mui/material/styles'
+import InputBase from '@mui/material/InputBase'
+import SearchIcon from '@mui/icons-material/Search'
+
 // ** Contexts
 import {useAuthContext} from '../contexts/authContext'
 
 interface IProps {
     logout: () => void
-    }
+}
+
+const Search = styled('div')(({theme}) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+        backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(1),
+        width: 'auto',
+    },
+}))
+
+const SearchIconWrapper = styled('div')(({theme}) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+}))
+
+const StyledInputBase = styled(InputBase)(({theme}) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            width: '12ch',
+            '&:focus': {
+                width: '20ch',
+            },
+        },
+    },
+}))
 
 // const NetflixAppBar = ({logout}: IProps): JSX.Element => {
 const NetflixAppBar = () => {
+    const navigate = useNavigate()
+    const [query, setQuery] = useState<any>()
     const {logout} = useAuthContext()
     const [appBarStyle, setAppBarStyle] = useState({
         background: 'transparent',
@@ -43,6 +91,13 @@ const NetflixAppBar = () => {
     }, [])
 
     const margin10 = {margin: 10}
+
+    const handleKeyPress = (e: any) => {
+        const keyEnter = 13
+        if (e.keyCode === keyEnter) {
+            navigate(`/search/${query}`)
+        }
+    }
 
     return (
         <AppBar style={appBarStyle}>
@@ -77,6 +132,17 @@ const NetflixAppBar = () => {
                         List
                     </Typography>
                 </Link>
+                <Search>
+                    <SearchIconWrapper>
+                        <SearchIcon />
+                    </SearchIconWrapper>
+                    <StyledInputBase
+                        placeholder="Search…"
+                        inputProps={{'aria-label': 'search'}}
+                        onKeyDown={handleKeyPress}
+                        onChange={(e)=> setQuery(e.target.value)}
+                    />
+                </Search>
                 <img
                     style={{marginLeft: 'auto'}}
                     className="nav__avatar"
