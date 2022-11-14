5a084d1be242edbae79c5e46dde27f2754250ae5
thierry <thierrypoupon@hotmail.com>
2022-11-14 12:45:56 +0100

    working on performance with dynamic import

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, {lazy, Suspense} from 'react'
 // ** MSW **
 import './mocks'
 // ** Components **
-import AuthApp from './AuthApp'
-import UnauthApp from './UnauthApp'
+// import AuthApp from './AuthApp'
+// import UnauthApp from './UnauthApp'
 // ** Contexts **
 import {useAuthContext} from './contexts/authContext'
 import AppProviders from './contexts'
+// ** Dynamic imports
+const AuthApp = lazy(() => import('./AuthApp'))
+const UnauthApp = lazy(() => import('./UnauthApp'))
 
 function App() {
     return (
@@ -18,7 +21,11 @@ function App() {
 
 const AppConsumer = () => {
     const {authUser} = useAuthContext()
-    return authUser ? <AuthApp /> : <UnauthApp />
+    return (
+        <Suspense fallback={<div>Chargement...</div>} >
+            {authUser ? <AuthApp /> : <UnauthApp />}
+        </Suspense>
+    )
 }
 
 export default App
