704dd5bec36997e7fb29a1cc77dd3ebd7cf93d37
thierry <thierrypoupon@hotmail.com>
2022-11-16 14:54:58 +0100

    MSW must run too in production

--- src/mocks/index.js
+++ src/mocks/index.js
@@ -1,4 +1,10 @@
 if (process.env.NODE_ENV === 'development') {
-    console.log("process.env.NODE_ENV === 'development'")
+    console.log("%c process.env.NODE_ENV === ", "color: green;", process.env.NODE_ENV)
+    module.exports = require('./browser')
+} else if (process.env.NODE_ENV === 'test') {
+    console.log("%c process.env.NODE_ENV === ", "color: tomato;", process.env.NODE_ENV)
+    //module.exports = require('./server')
+} else {
+    console.log("%c process.env.NODE_ENV === ", "color: yellow;", process.env.NODE_ENV )
     module.exports = require('./browser')
 }
