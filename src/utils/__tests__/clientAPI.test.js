fabec71dcc6a296c645baa4b5769cb4bb5acea74
thierry <thierrypoupon@hotmail.com>
2022-11-21 17:50:57 +0100

    working on unit tests clientAuth

--- src/utils/__tests__/clientAPI.test.js
+++ src/utils/__tests__/clientAPI.test.js
@@ -8,4 +8,22 @@ afterEach(() => server.resetHandlers())
 
 afterAll(() => server.close())
 
-test.todo('should return random integer')
+test('send a request to a endPoint', async () => {
+    const endPoint = 'fake-endpoint'
+    const resultRequest = {mockResult: 'TEST'}
+    const resultRequestFalse = {mockResult: 'TEST2'}
+
+    server.use(
+        rest.get(
+            `https://auth.service.mock.com/${endPoint}`,
+            (req, res, ctx) => {
+                return res(ctx.json(resultRequest))
+            },
+        ),
+    )
+
+    const result = await clientAuth(endPoint)
+    // console.log("result === ", result.data)
+    expect(result.data).toEqual(resultRequest)
+    // expect(result.data).toEqual(resultRequestFalse)
+})
