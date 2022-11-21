4350d0dfacd614408acf2369673b07b63ee016e4
thierry <thierrypoupon@hotmail.com>
2022-11-21 10:54:53 +0100

    unit tests helpers.ts

--- src/mocks/index.js
+++ src/mocks/index.js
@@ -3,7 +3,7 @@ if (process.env.NODE_ENV === 'development') {
     module.exports = require('./browser')
 } else if (process.env.NODE_ENV === 'test') {
     console.log("%c process.env.NODE_ENV === ", "color: tomato;", process.env.NODE_ENV)
-    //module.exports = require('./server')
+    module.exports = require('./server_for_run_tests')
 } else {
     console.log("%c process.env.NODE_ENV === ", "color: yellow;", process.env.NODE_ENV )
     module.exports = require('./browser')
