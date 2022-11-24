ea3e9511866dae9542d09176f17e7ec451cf3ccf
thierry <thierrypoupon@hotmail.com>
2022-11-24 12:34:05 +0100

    working on unit tests clientAuth

--- src/utils/__tests__/clientAPI.test.js
+++ src/utils/__tests__/clientAPI.test.js
@@ -27,3 +27,45 @@ test('send a request to a endPoint', async () => {
     expect(result.data).toEqual(resultRequest)
     // expect(result.data).toEqual(resultRequestFalse)
 })
+test('check the token passed as parameter', async () => {
+    const endPoint = 'fake-endpoint'
+    const resultRequest = {mockResult: 'TEST'}
+    const token = 'fake-token'
+    let request
+
+    server.use(
+        rest.get(
+            // `https://auth.service.mock.com/${endPoint}${token}`,
+            `https://auth.service.mock.com/${endPoint}`,
+            async (req, res, ctx) => {
+                request = req
+                // console.log('req request === ', request)
+                return res(ctx.json(resultRequest))
+            },
+        ),
+    )
+
+    const result = await clientAuth(endPoint, token)
+    expect(request.headers.get('authorization')).toBe(`Bearer ${token}`)
+})
+test('check when no token passed as parameter', async () => {
+    const endPoint = 'fake-endpoint'
+    const resultRequest = {mockResult: 'TEST'}
+    const token = 'undefined'
+    let request
+
+    server.use(
+        rest.get(
+            // `https://auth.service.mock.com/${endPoint}${token}`,
+            `https://auth.service.mock.com/${endPoint}`,
+            async (req, res, ctx) => {
+                request = req
+                // console.log('req request === ', request)
+                return res(ctx.json(resultRequest))
+            },
+        ),
+    )
+
+    const result = await clientAuth(endPoint)
+    expect(request.headers.get('authorization')).toBe(`${token}`)
+})
