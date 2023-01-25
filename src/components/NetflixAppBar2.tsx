b07b9b91d48e0055cd7ffeb364cc8c640b038547
thierry <thierrypoupon@hotmail.com>
2023-01-25 21:20:20 +0100

    :construction: working in progress on new appbar responsive

--- src/components/NetflixAppBar2.tsx
+++ src/components/NetflixAppBar2.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import {Link, useNavigate} from 'react-router-dom'
 import styled from 'styled-components'
 import SearchIcon from '@mui/icons-material/Search'
@@ -6,7 +6,28 @@ import SearchIcon from '@mui/icons-material/Search'
 const Button = styled.button``
 
 const Wrapper = styled.div`
+    width: 100%;
     display: flex;
+    padding: 0 12px;
+    z-index: 1111;
+    position: fixed;
+    flex-wrap: wrap;
+    font-size: 1.25rem;
+    min-height: 64px;
+    align-items: center;
+    letter-spacing: 0.0075em;
+    /* font-weight: 500; */
+    /* transition: 'background 4s ease-out' // ne fonctionne pas !!!! */
+`
+
+const Nav = styled.nav`
+    margin-left: auto;
+`
+
+const StyledLink = styled(Link)`
+    color: white;
+    padding: 0 5px;
+    text-decoration: none;
 `
 
 const ImgLogoNetflix = styled.img`
@@ -17,30 +38,64 @@ const ImgAvatarForLogout = styled.img`
     width: 30px;
 `
 
+const Actions = styled.div`
+    display: flex;
+    align-items: center;
+    margin-left: auto;
+`
+
 const NetflixAppBar2 = () => {
+    const [backgroundStyle, setBackgroundStyle] = useState<any>({
+        background: 'transparent',
+        transition: 'none'
+    })
+
+    useEffect(() => {
+        const onScroll = (e: any) => {
+            console.log(e.target.documentElement.scrollTop)
+            if (e.target.documentElement.scrollTop > 100) {
+                setBackgroundStyle({
+                    background: '#111',
+                    transition: 'background 2s ease-out'
+                })
+            } else {
+                setBackgroundStyle({
+                    background: 'transparent',
+                    transition: 'background 2s ease-out'
+                })
+            }
+        }
+        window.addEventListener('scroll', onScroll)
+
+        return () => window.removeEventListener('scroll', onScroll)
+    }, [])
+
     return (
         // <div className="nav">
-        <Wrapper className="nav">
+        <Wrapper style={backgroundStyle}>
             <Button className="nav_burger">***</Button>
             <ImgLogoNetflix src="images/netflix-logo.png" alt="Netflix" />
-            <nav className="nav__links">
-                <Link className="nav__link" to="/">
+            {/* <nav className="nav__links"> */}
+            <Nav>
+                <StyledLink className="nav__link" to="/">
                     Accueil
-                </Link>
-                <Link className="nav__link" to="/series">
+                </StyledLink>
+                <StyledLink className="nav__link" to="/series">
                     Series
-                </Link>
-                <Link className="nav__link" to="/movies">
+                </StyledLink>
+                <StyledLink className="nav__link" to="/movies">
                     Movies
-                </Link>
-                <Link className="nav__link" to="/news">
+                </StyledLink>
+                <StyledLink className="nav__link" to="/news">
                     News
-                </Link>
-                <Link className="nav__link" to="/list">
+                </StyledLink>
+                <StyledLink className="nav__link" to="/list">
                     List
-                </Link>
-            </nav>
-            <div className="nav__actions">
+                </StyledLink>
+            </Nav>
+            {/* </nav> */}
+            {/* <div className="nav__actions"> */}
+            <Actions>
                 <Link className="nav__action" to="/">
                     search
                 </Link>
@@ -48,7 +103,8 @@ const NetflixAppBar2 = () => {
                     src="/images/netflix-avatar.png"
                     alt="netflix avatar"
                 />
-            </div>
+            </Actions>
+            {/* </div> */}
         </Wrapper>
         // </div>
     )
