dd0ce94d21d8ff2823d33794e3daa6ecd2853aac
thierry <thierrypoupon@hotmail.com>
2023-02-22 18:21:34 +0100

    :construction: deplace logic of login to the new file clientToNetflixApi.ts

--- src/services/clientToNetflixApi.ts
+++ src/services/clientToNetflixApi.ts
@@ -1,1 +1,34 @@
-export {}
+type LoginData = {
+    userName: string
+    password: string
+}
+
+const clientSendsRequestsToTheNetflixApi = async (
+    endPoint: 'login' | 'register',
+    // data: {userName: string; password: string},
+    data: LoginData,
+): Promise<any> => {
+    return fetch(`/netflixAPI/${endPoint}`, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {'Content-Type': 'application/json'},
+    }).then(async response => {
+        const data = await response.json()
+        if (response.ok) {
+            return data
+        } else {
+            return Promise.reject(data)
+            /*
+             * or same result
+             * throw new Error(data.message)
+             */
+        }
+    })
+}
+
+export const login = async ({userName, password}: LoginData) => {
+    return await clientSendsRequestsToTheNetflixApi('login', {
+        userName,
+        password,
+    })
+}
