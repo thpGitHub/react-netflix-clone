04a490142eabf432f0bb1975c3d3c10272ddaa16
thierry <thierrypoupon@hotmail.com>
2023-02-23 18:56:19 +0100

    :construction:I commented the future options of worker.start(): I must define the scope for requests captured by MSW in order to avoid warnings in the console

--- src/mocks/browser.js
+++ src/mocks/browser.js
@@ -4,3 +4,13 @@ import { handlers } from './handlers'
 export const worker = setupWorker(...handlers)
 
 worker.start()
+
+// worker.start({
+//     serviceWorker: {
+//       options: {
+//         // Narrow the scope of the Service Worker to intercept requests
+//         // only from pages under this path.
+//         scope: '/product'
+//       }
+//     }
+//   })
