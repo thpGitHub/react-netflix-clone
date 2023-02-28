13bae4857693e651405626f6f667a20dae27fcd8
thierry <thierrypoupon@hotmail.com>
2023-02-28 18:29:18 +0100

    :recycle: bypass any requests that do not match one of the defined request handlers

--- src/mocks/browser.js
+++ src/mocks/browser.js
@@ -3,14 +3,19 @@ import { handlers } from './handlers'
 // This configures a Service Worker with the given request handlers.
 export const worker = setupWorker(...handlers)
 
-worker.start()
+// worker.start() 
+
+worker.start({ onUnhandledRequest: 'bypass' });
 
 // worker.start({
 //     serviceWorker: {
+//       url: '/mockServiceWorker.js',
 //       options: {
-//         // Narrow the scope of the Service Worker to intercept requests
-//         // only from pages under this path.
-//         scope: '/product'
+//         scope: '/',
+//         // exclude the image.tmdb.org domain from being intercepted by MSW
+//         exclude: [
+//           'https://image.tmdb.org/*'
+//         ]
 //       }
 //     }
-//   })
+//   });
