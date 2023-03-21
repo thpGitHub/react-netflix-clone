0bbd4222b550f50912b689698b1dedcb6f9761d4
thierry <thierrypoupon@hotmail.com>
2023-03-21 17:30:28 +0100

    :lipstick: add react-hook-form and zod in this component

--- src/components/LoginRegister.tsx
+++ src/components/LoginRegister.tsx
@@ -6,12 +6,26 @@ import Checkbox from '@mui/material/Checkbox'
 import FormGroup from '@mui/material/FormGroup'
 import TextField from '@mui/material/TextField'
 import FormControlLabel from '@mui/material/FormControlLabel'
-import Dialog from '@mui/material/Dialog'
+import Dialog, {DialogProps} from '@mui/material/Dialog'
 import DialogTitle from '@mui/material/DialogTitle'
 import DialogActions from '@mui/material/DialogActions'
 import DialogContent from '@mui/material/DialogContent'
 // ** Contexts **
 import {useAuthContext} from '../contexts/authContext'
+//** hook form and zod **/
+import {useForm, Controller} from 'react-hook-form'
+import {z} from 'zod'
+import {zodResolver} from '@hookform/resolvers/zod'
+
+const schema = z.object({
+    email: z.string().nonempty('Email is required').email('Invalid email'),
+    password: z
+        .string()
+        .min(4, 'Password must be at least 4 characters long')
+        .max(32, 'Password must be at most 32 characters long'),
+})
+
+type FormValues = z.infer<typeof schema>
 
 type AuthData = {
     userName: string
@@ -27,53 +41,75 @@ type Props = {
 }
 
 const FormLogin = ({createLoginCount = true, login, register}: Props) => {
-    const [userName, setUserName] = useState<string>('')
-    const [password, setPassword] = useState<string>('')
+    const {handleSubmit, control} = useForm<FormValues>({
+        resolver: zodResolver(schema),
+        defaultValues: {email: '', password: ''},
+    })
 
     const labelButton = createLoginCount ? 'Inscrivez-vous' : 'Connexion'
 
+    const onSubmit = ({email, password}: FormValues) => {
+        console.log({email: email, password: password})
+        !createLoginCount
+            ? login({
+                  userName: email,
+                  password: password,
+              })
+            : register({
+                  userName: email,
+                  password: password,
+              })
+    }
+
     return (
-        <form autoComplete="off">
-            <TextField
-                id="filled-basic-username"
-                type="email"
-                label="Email ou numéro de téléphone"
-                margin="dense"
-                variant="filled"
-                onChange={e => setUserName(e.target.value)}
-                autoFocus
-                fullWidth
-            />
-            <TextField
-                id="filled-basic-password"
-                type="password"
-                label="Mot de passe"
-                margin="dense"
-                onChange={e => setPassword(e.target.value)}
-                variant="filled"
-                fullWidth
-            />
-            {createLoginCount ? (
-                <>
-                    <Button
-                        variant="contained"
+        <form autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
+            <Controller
+                name="email"
+                control={control}
+                render={({
+                    field: {onChange, onBlur, value, ref},
+                    fieldState: {error},
+                }) => (
+                    <TextField
+                        error={!!error}
+                        label="Email"
+                        value={value}
+                        margin="normal"
+                        onBlur={onBlur}
+                        variant="outlined"
+                        inputRef={ref}
+                        onChange={onChange}
+                        autoFocus
                         fullWidth
-                        onClick={() => register({userName, password})}
-                    >
-                        {labelButton}
-                    </Button>
-                </>
-            ) : (
-                <>
-                    <Button
-                        variant="contained"
+                        helperText={error ? error.message : null}
+                    />
+                )}
+            />
+            <Controller
+                name="password"
+                control={control}
+                render={({
+                    field: {onChange, onBlur, value, ref},
+                    fieldState: {error},
+                }) => (
+                    <TextField
+                        error={!!error}
+                        label="Password"
+                        value={value}
+                        margin="normal"
+                        onBlur={onBlur}
+                        variant="outlined"
+                        inputRef={ref}
+                        onChange={onChange}
                         fullWidth
-                        onClick={() => login({userName, password})}
-                    >
-                        {labelButton}
-                    </Button>
-                </>
-            )}
+                        helperText={error ? error.message : null}
+                    />
+                )}
+            />
+
+            <Button type="submit" variant="contained" fullWidth>
+                {labelButton}
+            </Button>
 
             <FormGroup>
                 <FormControlLabel
@@ -86,10 +122,10 @@ const FormLogin = ({createLoginCount = true, login, register}: Props) => {
 }
 
 const PopupLogin = ({signUp = false}) => {
-    // const {login, register, authError: error} = useContext(AuthContext)
     const {login, register, authError: error} = useAuthContext()
     const [createLogin, setCreateLogin] = useState(signUp)
     const [open] = React.useState(true)
+    const [maxWidth] = React.useState<DialogProps['maxWidth']>('xs')
 
     const labelTitle = createLogin ? 'Inscrivez-vous' : 'Connexion'
 
@@ -102,7 +138,11 @@ const PopupLogin = ({signUp = false}) => {
 
     return (
         <div>
-            <Dialog open={open} style={{backgroundColor: 'transparent'}}>
+            <Dialog
+                open={open}
+                maxWidth={maxWidth}
+                sx={{backgroundColor: 'transparent'}}
+            >
                 <DialogTitle>{labelTitle}</DialogTitle>
                 <DialogContent>
                     <FormLogin
